300fba7c1a03ec880d86f3451624ac68a990f623
LeandroJoseDev <leandrojosedev@gmail.com>
2024-01-14 16:46:30 -0300

    desafioClassificador

---
+++ index.js
@@ -0,0 +1,36 @@
+let Heroi = "THOR";
+let XP = 0;
+
+if (XP <= 1000) {
+    console.log("--------------------------------------------");
+    console.log("O heroi **", Heroi, "** esta no nivel ** FERRO **");
+    console.log("-----------------------------------------------");
+} else if (XP <= 2000) {
+    console.log("---------------------------------------------------");
+    console.log("O heroi **", Heroi, "** esta no nivel ** BRONZE **");
+    console.log("---------------------------------------------------");
+} else if (XP <= 5000) {
+    console.log("---------------------------------------------------");
+    console.log("O heroi **", Heroi, "** esta no nivel ** PRATA **");
+    console.log("---------------------------------------------------");
+} else if (XP <= 7000) {
+    console.log("---------------------------------------------------");
+    console.log("O heroi **", Heroi, "** esta no nivel ** OURO **");
+    console.log("---------------------------------------------------");
+} else if (XP <= 8000) {
+    console.log("---------------------------------------------------");
+    console.log("O heroi **", Heroi, "** esta no nivel ** PLATINA **");
+    console.log("---------------------------------------------------");
+} else if (XP <= 9000) {
+    console.log("---------------------------------------------------");
+    console.log("O heroi **", Heroi, "** esta no nivel ** ASCENDENTE **");
+    console.log("---------------------------------------------------");
+} else if (XP <= 10000) {
+    console.log("---------------------------------------------------");
+    console.log("O heroi **", Heroi, "** esta no nivel ** IMORTAL **");
+    console.log("---------------------------------------------------");
+} else if (XP >= 10001) {
+    console.log("---------------------------------------------------");
+    console.log("O heroi **", Heroi, "** esta no nivel ** RADIANTE **");
+    console.log("---------------------------------------------------");
+}
